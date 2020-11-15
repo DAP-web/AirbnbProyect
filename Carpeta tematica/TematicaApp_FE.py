ab192089d6261d0e8db6f641468cd5dd79d260a7
victoriaZepeda <66028491+victoriaZepeda@users.noreply.github.com>
2020-11-14 22:09:41 -0600

    Tematica

---
+++ Carpeta tematica/TematicaApp_FE.py
@@ -0,0 +1,34 @@
+from Tematica_BE import (
+    connection,
+    getAllTematicas,
+    addTematica,
+    updateTematica,
+    deleteTematica
+)
+
+print("Inicializando la app de Airbnb Tematica")
+while True:
+    Menu = """\nElija una de las siguientes opciones:
+    0-Salir de la app
+    1-Recuperar todas las tematicas
+    2-Ingresar una nueva tematica
+    3-Actualizar tematica
+    4-Eliminar tematica\n"""
+    print("-"*100)
+    print(Menu)
+    print("-"*100)
+    option = int(input("Opción: "))
+
+    if option == 0:
+        print("\nDeteniendo la aplicación de Airbnb Tematica")
+        connection.close()
+        break
+    if option == 1:
+        getAllTematicas()
+    if option == 2:
+        addTematica()
+    if option == 3:
+        updateTematica()
+    if option == 4:
+        deleteTematica()
+        
---
+++ Carpeta tematica/Tematica_BE.py
@@ -0,0 +1,66 @@
+from prettytable import PrettyTable
+from DB_Tematica_BE import (
+    connection,
+    getTematica,
+    insertTematica,
+    searchTematicaById,
+    updateTematicaBD,
+    traerIDTematica,
+    deleteTematicaDB
+)
+
+def getAllTematicas():
+    result = getTematicas()
+
+    table = PrettyTable()
+    table.field_names = ["IdTematica", "NombreTematica", "Descripcion"]
+
+    for tematica in result:
+        table.add_row([tematica["IdTematica"],tematica["NombreTematica"],tematica["Descripcion"]])
+
+    print(table)
+    table.clear()
+
+def addTematica():
+    print("\nAdding a new tematica...")
+    tematicaname = input("\nNombre de tematica: ")
+    description = input("\nDescripción: ")
+
+    insertTematica(tematicaname,description)
+    idtematica=traerIDTematica(tematicaname,description)
+
+    print("\nSu tematica se ha creado con éxito.\n")
+    print(f"Su descripción es {idtematica}.\n")
+    getAllTematicas()
+
+def updateTematica():
+    print("\nUpdating an existing tematica...")
+    id = int(input("\nID de la tematica a actualizar: "))
+
+    tematica = searchTematicaById(id)
+
+    update = int(input("Update Name? 0-No - 1-Yes "))
+    if update == 1:
+        print(f"Old Name: {tematica['NombreTematica']}")
+        tematicaname = input("New Name: ")
+    else:
+        tematicaname = tematica["NombreTematica"]
+
+    update = int(input("Update Descripcion? 0-No - 1-Yes "))
+    if update == 1:
+        print(f"Old Description: {tematica['Descripcion']}")
+        code = input("New Description: ")
+    else:
+        code = tematica["Descripcion"]
+
+    updateTematicaBD(id,tematicaname,description)
+    print("\nLos cambios se han efectuado con éxito.")
+    getAllTematicas()
+
+def deleteTematica():
+    print("Deleting tematica...")
+    id = int(input("ID of tematica to delete: "))
+
+    deleteTematicaDB(id)
+    print("La temática se ha removido con éxito.")
+    getAllTematicas()
