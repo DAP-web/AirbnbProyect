c870cd47d3e6697bcfe56195e7339b0cb7107b9c
gabsbeltran001 <47165580+gabsbeltran001@users.noreply.github.com>
2020-11-27 09:34:29 -0600

    accesibilidad clase

--- Segundo Avance/Formularios ABC/Accesiblidad_BE.py
+++ Segundo Avance/Formularios ABC/Accesiblidad_BE.py
@@ -1,58 +1,56 @@
 from prettytable import PrettyTable
-from DB_Accesibilidad_BE import (
-    connection,
-    getAccessibilities,
-    insertAccessibility,
-    searchAccessibilityById,
-    updateAccesibilityBD,
-    traerIDAccessibility,
-    deleteAccessibilityDB
-)
+from DB_Accesibilidad_BE import AccesibilidadBD
 
-def getAllAccessibilities():
-    result = getAccessibilities()
 
-    table = PrettyTable()
-    table.field_names = ["IdAccesibilidad", "Nombre"]
 
-    for accesibilidad in result:
-        table.add_row([accesibilidad["idAccesibilidad"],accesibilidad["Nombre"]])
+class accesibilidad:
+    self.dbreservas=reservasBD()
+        self.dbresidencia=ResidenciaDB()
 
-    print(table)
-    table.clear()
+    def getAllAccessibilities():
+        result = getAccessibilities()
 
-def addAccessibility():
-    print("\nAdding a new accessibility...")
-    accessibilityname = input("\nNombre de la accesibilidad: ")
-    
-    insertAccessibility(accessibilityname)
-    idaccesibilidad=traerIDAccessibility(accessibilityname)
+        table = PrettyTable()
+        table.field_names = ["IdAccesibilidad", "Nombre"]
 
-    print("\nSu accesibilidad se ha creado con éxito.\n")
-    print(f"Su código de accesibilidad único es {idaccesibilidad}.\n")
-    getAllAccessibilities()
+        for accesibilidad in result:
+            table.add_row([accesibilidad["idAccesibilidad"],accesibilidad["Nombre"]])
 
-def updateAccessibility():
-    print("\nUpdating an existing accessibility...")
-    id = int(input("\nID de la accesibilidad a actualizar: "))
+        print(table)
+        table.clear()
 
-    accesibilidad = searchAccessibilityById(id)
+    def addAccessibility():
+        print("\nAdding a new accessibility...")
+        accessibilityname = input("\nNombre de la accesibilidad: ")
+        
+        insertAccessibility(accessibilityname)
+        idaccesibilidad=traerIDAccessibility(accessibilityname)
 
-    update = int(input("Update Name? 0-No - 1-Yes "))
-    if update == 1:
-        print(f"Old Name: {accesibilidad['Nombre']}")
-        accessibilityname = input("New Name: ")
-    else:
-        accessibilityname = accesibilidad["Nombre"]
+        print("\nSu accesibilidad se ha creado con éxito.\n")
+        print(f"Su código de accesibilidad único es {idaccesibilidad}.\n")
+        getAllAccessibilities()
 
-    updateAccesibilityBD(id,accessibilityname)
-    print("\nLos cambios se han efectuado con éxito.")
-    getAllAccessibilities()
+    def updateAccessibility():
+        print("\nUpdating an existing accessibility...")
+        id = int(input("\nID de la accesibilidad a actualizar: "))
 
-def deleteAccessibility():
-    print("Deleting accessibility...")
-    id = int(input("ID of accessibility to delete: "))
+        accesibilidad = searchAccessibilityById(id)
 
-    deleteAccessibilityDB(id)
-    print("La accesibilidad se ha removido con éxito.")
-    getAllAccessibilities()
+        update = int(input("Update Name? 0-No - 1-Yes "))
+        if update == 1:
+            print(f"Old Name: {accesibilidad['Nombre']}")
+            accessibilityname = input("New Name: ")
+        else:
+            accessibilityname = accesibilidad["Nombre"]
+
+        updateAccesibilityBD(id,accessibilityname)
+        print("\nLos cambios se han efectuado con éxito.")
+        getAllAccessibilities()
+
+    def deleteAccessibility():
+        print("Deleting accessibility...")
+        id = int(input("ID of accessibility to delete: "))
+
+        deleteAccessibilityDB(id)
+        print("La accesibilidad se ha removido con éxito.")
+        getAllAccessibilities()
