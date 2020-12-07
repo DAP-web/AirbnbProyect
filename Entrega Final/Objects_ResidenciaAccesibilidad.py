36a1dce345528cf589191609f3d6481081d78447
gabsbeltran001 <47165580+gabsbeltran001@users.noreply.github.com>
2020-12-06 22:17:40 -0600

    Create Objects_ResidenciaAccesibilidad.py

---
+++ Entrega Final/Objects_ResidenciaAccesibilidad.py
@@ -0,0 +1,10 @@
+class RAccesibilidadbj:
+    def __init__(self,idAccesibilidad,idResidencia,id):
+        self.id=id
+        self.idAccesibilidad = idAccesibilidad
+        self.idResidencia = idResidencia
+    
+    def __init__(self,idAccesibilidad,idResidencia,id=0):
+        self.id=id
+        self.idAccesibilidad = idAccesibilidad
+        self.idResidencia = idResidencia
